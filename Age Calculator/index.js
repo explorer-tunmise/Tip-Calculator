7635cb3e1119193ded4434b9f2bc6ad0a8d1ae83
Tunmise Adewole <adewoletunmise2000@gmail.com>
2026-02-09 23:04:40 +0100

    Made progress on how to collect date input and work with date methods

--- Age Calculator/index.js
+++ Age Calculator/index.js
@@ -1,11 +1,12 @@
 // Selecting elements in DOM
-let birthDate = document.getElementById("birthdate");
+const birthDate = document.getElementById("birthdate");
 let ageResult = document.getElementById("age-Result");
 
+birthDate.addEventListener("change", () => {
+ let dateInput = birthDate.value;
+ let time = new Date(dateInput);
+ let birthYear = time.getFullYear();
+ console.log(birthYear);
+});
+
 console.log(birthDate);
-
-function globalWindow () {
-    return this;
-}
-
-console.log(globalWindow());
