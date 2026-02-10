72eb35a7edb1ce20c42ad7e0194f7bd8279c8779
Tunmise Adewole <adewoletunmise2000@gmail.com>
2026-02-10 23:59:44 +0100

    Done

--- Age Calculator/index.js
+++ Age Calculator/index.js
@@ -1,12 +1,16 @@
 // Selecting elements in DOM
 const birthDate = document.getElementById("birthdate");
-let ageResult = document.getElementById("age-Result");
+let ageResult = document.getElementById("age-result");
+
+// Getting current year
+let currentYear = new Date().getFullYear();
+
 
 birthDate.addEventListener("change", () => {
  let dateInput = birthDate.value;
  let time = new Date(dateInput);
  let birthYear = time.getFullYear();
- console.log(birthYear);
+ let age = currentYear - birthYear;
+ console.log(age);
+ ageResult.innerText = `Your age is: ${age}`;
 });
-
-console.log(birthDate);
